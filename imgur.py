02f1bed0bfb540c283db08ab806e32b8e369b791
Kenhchs <hiiamfineandyou@gmail.com>
2022-06-10 01:21:01 +0800

    update imgur.py and auth.ini

--- imgur.py
+++ imgur.py
@@ -7,6 +7,8 @@ config = configparser.ConfigParser()
 config.read('auth.ini')
 client_id = config.get('credentials', 'client_id')
 client_secret = config.get('credentials', 'client_secret')
+imgur_Access_Token = 'Bearer ' + str(config.get('credentials', 'imgur_Access_Token'))
+print(imgur_Access_Token)
 
 def upload_image(path):
   image = pyimgur.Imgur(client_id)
@@ -19,7 +21,7 @@ def upload_vedio(path):
   url = "https://api.imgur.com/3/upload"
   payload = {'album': 'ALBUMID','type': 'file', 'disable_audio': '0'}
   files = [('video', open(path,'rb'))]
-  headers = {'Authorization': 'Bearer 7ba88725fa66d303e7cba56919081bd120d71746'}
+  headers = {'Authorization': imgur_Access_Token}
   response = requests.request("POST", url, headers=headers, data = payload, files = files)
   data = response.json()
   print(data["data"]["link"])
